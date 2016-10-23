66f05124c4da7a74192cb7b00dceae89f7a2096d
David Mckoy <dmckoy85@gmail.com>
2016-10-23 00:11:58 -0400

    Setup show filtering

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,16 +2,34 @@ const React = require('react');
 const ShowCard = require('./ShowCard');
 const data = require('../public/data');
 
-const Search = () => {
-  return (
-    <div className="container">
-      <div className="shows">
-        {data.shows.map((show) => {
-          return <ShowCard {...show} key={show.imdbID} />;
-        })}
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    };
+  },
+  handleSearchTerm (e) {
+    this.setState({searchTerm: e.target.value});
+  },
+  render () {
+    return (
+      <div className="container">
+        <header className="header">
+          <h1 className="brand">svideo</h1>
+          <input value={this.state.searchTerm} className="search-input"
+            type="text" placeholder="Search" onChange={this.handleSearchTerm} />
+        </header>
+        <div className="shows">
+          {data.shows.reduce((acc, show) => {
+            if (`${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0) {
+              acc.push(<ShowCard {...show} key={show.imdbID} />);
+            }
+            return acc;
+          }, [])}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+});
 
 module.exports = Search;
