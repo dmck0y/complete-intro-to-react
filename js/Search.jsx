3de3a135b8d46c7574c6e0b61c5db1d7fd701c24
David Mckoy <dmckoy85@gmail.com>
2016-10-22 20:16:31 -0400

    Add search page

---
+++ js/Search.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const shows = require('../public/data');
+
+const Search = () => {
+  return (
+    <pre>
+      <code>
+        {JSON.stringify(shows, null, 4)}
+      </code>
+    </pre>
+  );
+};
+
+module.exports = Search;
