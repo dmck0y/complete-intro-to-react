9d11049afe18d9ce5c07175229decb89086d9eb9
dmck0y <dmckoy85@gmail.com>
2016-10-29 10:42:23 -0400

    add detials component

---
+++ js/Details.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const Details = React.createClass({
+  render () {
+    return (
+      <div className="container">
+        asfasf
+      </div>
+    );
+  }
+});
+
+module.exports = Details;
